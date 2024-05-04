d9d0c8894cd3c1e9ab827590f5bb0aea413ffe74
Victor Freire <128263774+victorfreireavfs@users.noreply.github.com>
2024-05-04 20:11:32 -0300

    Data Binding
    -incluindo funções aos inputs
    -alterando o html em tempo real
    -methods{} executar funções

--- 1_instalação_vue/src/app.js
+++ 1_instalação_vue/src/app.js
@@ -1,9 +1,30 @@
 const MeuNome = {
     data() {
         return{
-            nome: "Victor Freire",
-            idade: 30
+            idade:"",
+            nome: "",
+            input_idade: "",
+            input_name: "",
         }
+    },
+
+
+    // v-on:click  = nome do submit
+    // (e) = transforma em elemento  
+    methods:{
+        // pega o nome do click e resgata o evento
+        submitForm(e){
+            // ação para esse evento: preventDefault(não deixa o form ser enviado pro serv)
+            e.preventDefault();
+            
+            // this = faz referência ao dado especifico
+            console.log(this.input_name);
+            console.log(this.input_idade);
+
+            this.nome = this.input_name;
+            this.idade = this.input_idade;
+        }
+
     }
 }
 
