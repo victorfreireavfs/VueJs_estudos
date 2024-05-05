f7d4ee538481200d971a2fdd208f4c39fe0ee6e1
Victor Freire <128263774+victorfreireavfs@users.noreply.github.com>
2024-05-05 15:16:32 -0300

    Métodos

--- 2_/src/components/Info.vue
+++ 2_/src/components/Info.vue
@@ -9,7 +9,10 @@
             <li>Python</li>
         </ul>
         <p v-if="3 > 2">Testando</p>
-        <p v-show="mostar_email">Meu email é:  {{ email }}</p>
+        <div>
+            <button @click="show_email">{{ texto_botao }}</button>
+        </div>
+        <p v-show="mostrar_email">Meu email é:  {{ email }}</p>
         <Picture/>
         <p>Para acessar meu portifólio basta <a v-bind:href="meu_link" target="_blank">clicar aqui</a></p>
     </div>
@@ -23,13 +26,25 @@ export default {
     components: {
         Picture
     },
-
+    
     data(){
         return{
             esta_trabalhando: false,
             mostar_email: false,
             email:"victoravgs@gmail.com",
-            meu_link:"https://github.com/victorfreireavfs/devmedia-estudos"
+            meu_link:"https://github.com/victorfreireavfs/devmedia-estudos",
+            texto_botao:"Mostrar email."
+        }
+    },
+    
+    methods:{
+    show_email(){
+            this.mostrar_email = !this.mostrar_email
+            if(!this.mostrar_email){
+                this.texto_botao='Mostrar email.'
+            } else{
+                this.texto_botao = 'Ocultar email.'
+            }
         }
     }
 }
