54c55aef6c299740285f657a24be6833d40f491b
Victor Freire <128263774+victorfreireavfs@users.noreply.github.com>
2024-05-05 11:12:13 -0300

    Criando primeiro componente
    - criando componente e exportando
    - importando no app root
    - exibindo através da tamplate com o nome do componente
    - expecificar o componente no export e chamar o componente no import do root

---
+++ 2_/src/components/PrimeiroComponente.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Olá Vue!</h1>
+        <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
+        <p>A chave do App é: {{ test }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PrimeiroComponente',
+    data(){
+        return{
+            nome: "Victor",
+            profissao: "Programador Jr.",
+            test: "Teste"           
+        }
+    },
+
+
+}
+</script>
