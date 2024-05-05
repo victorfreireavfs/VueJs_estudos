74b6f7e7a545cc7370e97f31e18af6c1f7095b6a
Victor Freire <128263774+victorfreireavfs@users.noreply.github.com>
2024-05-05 11:44:03 -0300

    Entendendo LifeCycleHooks
    - criando um componente que modifique o projeto em tempo real assim que iniciado.

--- 2_/src/App.vue
+++ 2_/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <PrimeiroComponente/>
+    <LifeCycle/>
     
   </div>
 </template>
 
 <script>
   import PrimeiroComponente from './components/PrimeiroComponente.vue'
+  import LifeCycle from './components/LifeCycle.vue'
   
   export default{
-      name: "App",
-      components: {
-          PrimeiroComponente
-      }
-        }
+    name: "App",
+    components: {
+      PrimeiroComponente,
+      LifeCycle
+    }
+  }
 </script>
---
+++ 2_/src/components/LifeCycle.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>Meu nome é : {{ nome }}</h1>
+    </div>
+</template>
+
+<script>
+// estudar arrow functions
+export default {
+    name: 'LifeCycle',
+    
+    data() {
+        return{
+            nome: "ainda não sei!"
+        }
+    },
+    
+    created() {
+        setTimeout(()=>{
+            this.nome="Victor"
+        }, 1000)
+    },
+    
+    mounted() {
+         setTimeout(()=>{
+            this.nome="João"
+        }, 2000)
+    }
+}
+</script>
