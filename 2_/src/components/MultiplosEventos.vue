6de05f3ef31a598a406c6b5fbaf343d01dbe5f5e
Victor Freire <128263774+victorfreireavfs@users.noreply.github.com>
2024-05-05 19:42:07 -0300

    Múltiplos eventos
    - @click pode ativar multiplos eventos separando-os pór vírgula
    -métodos são chamados na seção methods

---
+++ 2_/src/components/MultiplosEventos.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <button @click="primeiro($event), segundo($event), terceiro()">Ativar Multiplos Eventos</button>
+        <p>{{ multiplos_eventos }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MultiplosEventos',
+    
+    data(){
+        return{
+            multiplos_eventos: ""
+        }
+    }, 
+
+    methods:{
+        primeiro(e){
+            this.multiplos_eventos = "Primeiro evento ativado"
+        },
+        segundo(e){
+            setTimeout(() => this.multiplos_eventos = "Segundo evento", 500)
+        },
+        terceiro(){
+            setTimeout(() => this.multiplos_eventos = "", 1500)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    div{
+        margin:100px;
+    }
+</style>
--- 2_/src/components/PrimeiroComponente.vue
+++ 2_/src/components/PrimeiroComponente.vue
@@ -2,13 +2,19 @@
     <div>
         <h1>Olá Vue!</h1>
         <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
-        <p>A chave do App é: {{ test }}</p>
+        <MultiplosEventos />
     </div>
 </template>
 
 <script>
+import MultiplosEventos from './MultiplosEventos.vue';
+
 export default {
+
     name: 'PrimeiroComponente',
+    components: {
+        MultiplosEventos
+    },  
     data(){
         return{
             nome: "Victor",
@@ -16,7 +22,5 @@ export default {
             test: "Teste"           
         }
     },
-
-
 }
 </script>
