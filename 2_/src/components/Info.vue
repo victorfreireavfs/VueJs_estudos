7d0ee7ecd007c3582681cfe6a6ecce6a4f2857e8
Victor Freire <128263774+victorfreireavfs@users.noreply.github.com>
2024-05-05 14:34:31 -0300

    Atributos
    - adicionando atributos de forma dinâmica
    - : antes dos atributos do elemento para incluir tais funções do script

--- 2_/src/components/Info.vue
+++ 2_/src/components/Info.vue
@@ -1,18 +1,36 @@
 <template>
      <div>
-        
-        <p>Estou trabalhando no momento.</p>
+        <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+        <p v-else>Estou em busca de novas oportunidades.</p>
         <p>Utilizo as Seguintes tecnologias:</p>
         <ul>
             <li>JavaScript</li>
             <li>PHP</li>
             <li>Python</li>
         </ul>
+        <p v-if="3 > 2">Testando</p>
+        <p v-show="mostar_email">Meu email é:  {{ email }}</p>
+        <Picture/>
+        <p>Para acessar meu portifólio basta <a v-bind:href="meu_link" target="_blank">clicar aqui</a></p>
     </div>
 </template>
 
 <script>
+import Picture from "./Picture.vue"
+
 export default {
-    name: 'Info'
+    name: 'Info',
+    components: {
+        Picture
+    },
+
+    data(){
+        return{
+            esta_trabalhando: false,
+            mostar_email: false,
+            email:"victoravgs@gmail.com",
+            meu_link:"https://github.com/victorfreireavfs/devmedia-estudos"
+        }
+    }
 }
 </script>
---
+++ 2_/src/components/Picture.vue
@@ -0,0 +1,16 @@
+<template>
+    <img :src="avatar" :alt="descricao" :title="title">
+</template>
+
+<script>
+export default {
+    name: 'Picture',
+    data(){
+        return{
+            avatar:"/img/avatar.png",
+            descricao: "Victor Freire",
+            title: "minha foto usuário"
+        }
+    }
+}
+</script>
