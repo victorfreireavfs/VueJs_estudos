4e1926793a1f16d635c0240fdaaf7e5c1c3d8bbd
Victor Freire <128263774+victorfreireavfs@users.noreply.github.com>
2024-05-16 21:15:58 -0300

    Criando Nova Aplicação
    Criando novo app como parte dos estudos de Vue.Js pela DevMedia

---
+++ novo-projeto/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+      <img alt="Vue logo" src="./assets/logo.png">
+      <h1>{{ titulo }}</h1>
+      <p> {{ texto }}</p>
+    </div>
+ </template>
+
+<script>
+  export default {
+    name: 'App',
+    data(){
+      return{
+        titulo: "Hello Word",
+        texto: "Aplicação crianda com Vue CLI."
+      }
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
